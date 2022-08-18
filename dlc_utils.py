1893b64c57d2523574e1067417b081b0d42ecef7
dpeg22 <dpeg22@gmail.com>
2022-08-17 18:14:19 -0700

    add _convert_mp4 to dlc_utils

--- dlc_utils.py
+++ dlc_utils.py
@@ -2,6 +2,8 @@
 # DLC-utils copied from datajoint element-interface utils.py
 
 import pathlib
+import os
+import glob
 
 def find_full_path(root_directories, relative_path):
     """
@@ -67,3 +69,54 @@ def _to_Path(path):
     Handles one odd Windows/Linux incompatibility of the "\\"
     """
     return pathlib.Path(str(path).replace("\\", "/"))
+
+def _convert_mp4(filename: str, video_path: str, dest_path: str, 
+                videotype: str, count_frames=False, return_output=True):
+    '''converts video to mp4 using passthrough for frames
+    Parameters
+    ----------
+    filename: str
+        filename of video including suffix (e.g. .h264)
+    video_path: str
+        path of input video excluding filename
+    dest_path: str
+        path of output video excluding filename, but no suffix (e.g. no '.mp4')
+    videotype: str
+        str of filetype to convert to (currently only accepts 'mp4')
+    count_frames: bool
+        if True counts the number of frames in both videos instead of packets
+    return_output: bool
+        if True returns the destination filename
+    '''
+
+    import subprocess
+    orig_filename = filename
+    video_path = pathlib.Path(video_path + filename)
+    if videotype not in ['mp4']:
+        raise NotImplementedError
+    dest_filename = os.path.splitext(filename)[0]
+    if '.1' in dest_filename:
+        dest_filename = os.path.splitext(dest_filename)[0]
+    dest_path = pathlib.Path(dest_path + dest_filename + '.' + videotype)
+    convert_command = f"ffmpeg -vsync passthrough -i {video_path} -codec copy {dest_path}"
+    os.system(convert_command)
+    print(f'finished converting {filename}')
+    print(f'Checking that number of packets match between {orig_filename} and {dest_filename}')
+    num_packets = []
+    for ind, file in enumerate([video_path, dest_path]):
+        packets_command = ['ffprobe', '-v', 'error', '-select_streams',
+                        'v:0', '-count_packets', '-show_entries',
+                        'stream=nb_read_packets', '-of', 'csv=p=0', file]
+        frames_command = ['ffprobe', '-v', 'error', '-select_streams',
+                         'v:0', '-count_frames', '-show_entries',
+                         'stream=nb_read_frames', '-of', 'csv=p=0', file]
+        if count_frames:
+            p = subprocess.Popen(frames_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            p = subprocess.Popen(packets_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        num_packets.append(int(out.decode("utf-8").split("\n")[0]))
+    print(f'Number of packets in {orig_filename}: {num_packets[0]}, {dest_filename}: {num_packets[1]}')
+    assert num_packets[0] == num_packets[1]
+    if return_output:
+        return dest_path
