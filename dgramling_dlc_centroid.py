a64d8c9b91d974613ac05fa9843bfeb8909734f8
dpeg22 <dpeg22@gmail.com>
2022-08-31 18:30:45 -0700

    add methods for centroid calculation

--- dgramling_dlc_centroid.py
+++ dgramling_dlc_centroid.py
@@ -1,11 +1,12 @@
+import pynwb
+import os
+import sys
 import numpy as np
 import pandas as pd
 import math
 import datajoint as dj
 from datetime import datetime
-import pynwb
-import os
-import sys
+from functools import reduce
 from itertools import groupby
 from operator import itemgetter
 import bottleneck as bn
@@ -13,17 +14,24 @@ from typing import List, Dict, OrderedDict
 from pathlib import Path
 from spyglass.common.dj_helper_fn import fetch_nwb
 from spyglass.common.common_nwbfile import AnalysisNwbfile
+from position_tools import (
+    get_speed,
+    get_velocity,
+    interpolate_nan,
+)
+from position_tools.core import gaussian_smooth
 from .dgramling_dlc_position import DLCSmoothInterp
 from .dgramling_dlc_cohort import DLCSmoothInterpCohort
 from .dgramling_dlc_project import BodyPart
 
-schema = dj.schema('dgramling_dlc_centroid')
+schema = dj.schema("dgramling_dlc_centroid")
+
 
 @schema
 class DLCCentroidParams(dj.Manual):
-    """Parameters for calculating the centroid
-    """
-    definition="""
+    """Parameters for calculating the centroid"""
+
+    definition = """
     dlc_centroid_params_name: varchar(80) # name for this set of parameters
     ---
     max_separation = 9.0  : float   # max distance (in cm) between head LEDs
@@ -38,19 +46,20 @@ class DLCCentroidParams(dj.Manual):
 
 @schema
 class DLCCentroidSelection(dj.Manual):
-    """
-    """
-    definition="""
+    """ """
+
+    definition = """
     -> DLCSmoothInterpCohort
     -> DLCCentroidParams
     ---
     """
 
+
 @schema
 class DLCCentroid(dj.Computed):
-    """
-    """
-    definition="""
+    """ """
+
+    definition = """
     -> DLCCentroidSelection
     ---
     -> AnalysisNwbfile
@@ -58,44 +67,307 @@ class DLCCentroid(dj.Computed):
     """
 
     def make(self, key):
-        key['analysis_file_name'] = AnalysisNwbfile().create(
-                                    key['nwb_file_name'])
+        key["analysis_file_name"] = AnalysisNwbfile().create(key["nwb_file_name"])
         # Get labels to smooth from Parameters table
-        cohort_entries = (DLCSmoothInterpCohort.BodyPart & key)
-        part_dfs = []
-        for bodypart in cohort_entries.fetch('bodyparts'):
-            part_dfs.append((DLCSmoothInterpCohort.BodyPart & {**key, **{
-                "bodypart": bodypart}}).fetch1_dataframe())
+        cohort_entries = DLCSmoothInterpCohort.BodyPart & key
         params = (DLCCentroidParams() & key).fetch1()
-        
-        # centroid_bodyparts = (DLCCentroidCohortSelection.BodyPart & key).fetch()
-        query = DLCSmoothInterp() & key
-        bodyparts = (BodyPart & key).fetch('bodypart')
-        bodypart_dfs = {
-            entry['bodypart']: entry.fetch1_dataframe() for entry in query}
-        concat_df = pd.concat(
-            list(bodypart_dfs.values()),
-            axis=1,
-            keys=list(bodypart_dfs.keys()))
-        centroid_func = _key_to_func_dict(params['centroid_method'])
-        centroid = centroid_func(concat_df, **params['centroid_params'])
+        centroid_method = params["params"].pop("centroid_method")
+        bodyparts_avail = cohort_entries.fetch("bodyparts")
+        # TODO, generalize key naming
+        if centroid_method == "four_led_centroid":
+            if "green_LED" in params["params"]:
+                assert (
+                    params["params"]["green_LED"] in bodyparts_avail
+                ), f'{params["params"]["green_LED"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "A green led needs to be specified for the 4 led centroid method"
+                )
+            if "red_LED_L" in params["params"]:
+                assert (
+                    params["params"]["red_LED_L"] in bodyparts_avail
+                ), f'{params["params"]["red_LED_L"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "A left red led needs to be specified for the 4 led centroid method"
+                )
+            if "red_LED_C" in params["params"]:
+                assert (
+                    params["params"]["red_LED_C"] in bodyparts_avail
+                ), f'{params["params"]["red_LED_C"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "A center red led needs to be specified for the 4 led centroid method"
+                )
+            if "red_LED_R" in params["params"]:
+                assert (
+                    params["params"]["red_LED_R"] in bodyparts_avail
+                ), f'{params["params"]["red_LED_R"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "A right red led needs to be specified for the 4 led centroid method"
+                )
+            bodyparts_to_use = [
+                params["params"]["green_LED"],
+                params["params"]["red_LED_L"],
+                params["params"]["red_LED_C"],
+                params["params"]["red_LED_R"],
+            ]
 
+        elif centroid_method == "two_pt_centroid":
+            if "point1" in params["params"]:
+                assert (
+                    params["params"]["point1"] in bodyparts_avail
+                ), f'{params["params"]["point1"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "point1 needs to be specified for the 2 pt centroid method"
+                )
+            if "point2" in params["params"]:
+                assert (
+                    params["params"]["point2"] in bodyparts_avail
+                ), f'{params["params"]["point2"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "point2 needs to be specified for the 2 pt centroid method"
+                )
+            bodyparts_to_use = [params["params"]["point1"], params["params"]["point2"]]
+
+        elif centroid_method == "one_pt_centroid":
+            if "point1" in params["params"]:
+                assert (
+                    params["params"]["point1"] in bodyparts_avail
+                ), f'{params["params"]["point1"]} not a bodypart used in this model'
+            else:
+                raise ValueError(
+                    "point1 needs to be specified for the 1 pt centroid method"
+                )
+            bodyparts_to_use = [params["params"]["point1"]]
+
+        else:
+            raise ValueError("Please specify a centroid method to use.")
+        pos_df = pd.concat(
+            {
+                bodypart: (
+                    DLCSmoothInterpCohort.BodyPart & {**key, **{"bodypart": bodypart}}
+                ).fetch1_dataframe()
+                for bodypart in bodyparts_to_use
+            }
+        )
+        # TODO: not sure where to implement the distance check given potentially 4 bodyparts
+        # dist_between_LEDs = get_distance(back_LED, front_LED)
+        # is_too_separated = dist_between_LEDs >= max_LED_separation
+
+        # back_LED[is_too_separated] = np.nan
+        # front_LED[is_too_separated] = np.nan
+        centroid_func = _key_to_func_dict(params["centroid_method"])
+        centroid = centroid_func(pos_df, **params["params"])
 
 
 def four_led_centroid(pos_df, **params):
-    
-    centroid = None
-    return centroid
+    centroid = np.zeros(shape=(len(pos_df), 2))
+    idx = pd.IndexSlice
+    # TODO: this feels messy, clean-up
+    green_led = params.pop("green_LED", None)
+    red_led_C = params.pop("red_LED_C", None)
+    red_led_L = params.pop("red_LED_L", None)
+    red_led_R = params.pop("red_LED_R", None)
+    green_nans = pos_df.loc[:, idx[green_led, ("x", "y")]].isna().any(axis=1)
+    red_C_nans = pos_df.loc[:, idx[red_led_C, ("x", "y")]].isna().any(axis=1)
+    red_L_nans = pos_df.loc[:, idx[red_led_L, ("x", "y")]].isna().any(axis=1)
+    red_R_nans = pos_df.loc[:, idx[red_led_R, ("x", "y")]].isna().any(axis=1)
+    # TODO: implement checks to make sure not rewriting previously set index in centroid
+    # If all given LEDs are not NaN
+    all_good_mask = reduce(
+        np.logical_and, (~green_nans, ~red_C_nans, ~red_L_nans, ~red_R_nans)
+    )
+    centroid[all_good_mask] = (
+        (
+            pos_df.loc[idx[all_good_mask], idx[red_led_C, "x"]]
+            + pos_df.loc[idx[all_good_mask], idx[green_led, "x"]]
+        )
+        / 2,
+        (
+            pos_df.loc[idx[all_good_mask], idx[red_led_C, "y"]]
+            + pos_df.loc[idx[all_good_mask], idx[green_led, "y"]]
+        )
+        / 2,
+    )
+    # If all given LEDs are NaN
+    all_bad_mask = reduce(
+        np.logical_and, (green_nans, red_C_nans, red_L_nans, red_R_nans)
+    )
+    centroid[all_bad_mask, :] = np.nan
+    # If green LED is NaN, but red center LED is not
+    no_green_red_C = np.logical_and(green_nans, ~red_C_nans)
+    centroid[no_green_red_C] = [
+        *zip(
+            pos_df.loc[idx[no_green_red_C], idx[red_led_C, "x"]],
+            pos_df.loc[idx[no_green_red_C], idx[red_led_C, "y"]],
+        )
+    ]
+    # If green and red center LEDs are NaN, but red left and red right LEDs are not
+    no_green_no_red_C_red_L_red_R = reduce(
+        np.logical_and, (green_nans, red_C_nans, ~red_L_nans, ~red_R_nans)
+    )
+    centroid[no_green_no_red_C_red_L_red_R] = [
+        *zip(
+            (
+                pos_df.loc[idx[no_green_no_red_C_red_L_red_R], idx[red_led_L, "x"]]
+                + pos_df.loc[idx[no_green_no_red_C_red_L_red_R], idx[red_led_R, "x"]]
+            )
+            / 2,
+            (
+                pos_df.loc[idx[no_green_no_red_C_red_L_red_R], idx[red_led_L, "y"]]
+                + pos_df.loc[idx[no_green_no_red_C_red_L_red_R], idx[red_led_R, "y"]]
+            )
+            / 2,
+        )
+    ]
+    # If red center LED is NaN, but green, red left, and right LEDs are not
+    green_red_L_red_R_no_red_C = reduce(
+        np.logical_and, (~green_nans, red_C_nans, ~red_L_nans, ~red_R_nans)
+    )
+    midpoint = (
+        (
+            pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[red_led_L, "x"]]
+            + pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[red_led_R, "x"]]
+        )
+        / 2,
+        (
+            pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[red_led_L, "y"]]
+            + pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[red_led_R, "y"]]
+        )
+        / 2,
+    )
+    centroid[green_red_L_red_R_no_red_C] = [
+        *zip(
+            (
+                midpoint[0]
+                + pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[green_led, "x"]]
+            )
+            / 2,
+            (
+                midpoint[1]
+                + pos_df.loc[idx[green_red_L_red_R_no_red_C], idx[green_led, "y"]]
+            )
+            / 2,
+        )
+    ]
+    # If red center and left LED is NaN, but green and red right LED are not
+    green_red_R_no_red_C_no_red_L = reduce(
+        np.logical_and, (~green_nans, red_C_nans, red_L_nans, ~red_R_nans)
+    )
+    centroid[green_red_R_no_red_C_no_red_L] = [
+        *zip(
+            (
+                pos_df.loc[idx[green_red_R_no_red_C_no_red_L], idx[red_led_R, "x"]]
+                + pos_df.loc[idx[green_red_R_no_red_C_no_red_L], idx[green_led, "x"]]
+            )
+            / 2,
+            (
+                pos_df.loc[idx[green_red_R_no_red_C_no_red_L], idx[red_led_R, "y"]]
+                + pos_df.loc[idx[green_red_R_no_red_C_no_red_L], idx[green_led, "y"]]
+            )
+            / 2,
+        )
+    ]
+    # If red center and right LED is NaN, but green and red left LED are not
+    green_red_L_no_red_C_no_red_R = reduce(
+        np.logical_and, (~green_nans, red_C_nans, ~red_L_nans, red_R_nans)
+    )
+    centroid[green_red_L_no_red_C_no_red_R] = [
+        *zip(
+            (
+                pos_df.loc[idx[green_red_L_no_red_C_no_red_R], idx[red_led_L, "x"]]
+                + pos_df.loc[idx[green_red_L_no_red_C_no_red_R], idx[green_led, "x"]]
+            )
+            / 2,
+            (
+                pos_df.loc[idx[green_red_L_no_red_C_no_red_R], idx[red_led_L, "y"]]
+                + pos_df.loc[idx[green_red_L_no_red_C_no_red_R], idx[green_led, "y"]]
+            )
+            / 2,
+        )
+    ]
+    # If all red LEDs are NaN, but green LED is not
+    green_no_red = reduce(
+        np.logical_and, (~green_nans, red_C_nans, red_L_nans, red_R_nans)
+    )
+    centroid[green_no_red] = [
+        *zip(
+            pos_df.loc[idx[green_no_red], idx[green_led, "x"]],
+            pos_df.loc[idx[green_no_red], idx[green_led, "y"]],
+        )
+    ]
 
-def two_pt_centroid():
-    raise NotImplementedError
+    centroid_dict = {}
+    centroid_dict["position_x"] = centroid[0]
+    centroid_dict["position_y"] = centroid[1]
+    return centroid_dict
 
-def one_pt_centroid():
-    raise NotImplementedError
+
+def two_pt_centroid(pos_df, **params):
+    """Determines the centroid of 2 points."""
+    idx = pd.IndexSlice
+    centroid = np.zeros(shape=(len(pos_df), 2))
+    PT1 = params.pop("point1", None)
+    PT2 = params.pop("point2", None)
+    pt1_nans = pos_df.loc[:, idx[PT1, ("x", "y")]].isna().any(axis=1)
+    pt2_nans = pos_df.loc[:, idx[PT2, ("x", "y")]].isna().any(axis=1)
+    all_good_mask = np.logical_and(~pt1_nans, ~pt2_nans)
+    centroid[all_good_mask] = [
+        *zip(
+            (
+                pos_df.loc[idx[all_good_mask], idx[PT1, "x"]]
+                + pos_df.loc[idx[all_good_mask], idx[PT2, "x"]]
+            )
+            / 2,
+            (
+                pos_df.loc[idx[all_good_mask], idx[PT1, "y"]]
+                + pos_df.loc[idx[all_good_mask], idx[PT2, "y"]]
+            )
+            / 2,
+        )
+    ]
+    # If only point1 is good
+    pt1_mask = np.logical_and(~pt1_nans, pt2_nans)
+    centroid[pt1_mask] = [
+        *zip(
+            pos_df.loc[idx[pt1_mask], idx[PT1, "x"]],
+            pos_df.loc[idx[pt1_mask], idx[PT1, "y"]],
+        )
+    ]
+    # If only point2 is good
+    pt2_mask = np.logical_and(pt1_nans, ~pt2_nans)
+    centroid[pt2_mask] = [
+        *zip(
+            pos_df.loc[idx[pt2_mask], idx[PT2, "x"]],
+            pos_df.loc[idx[pt2_mask], idx[PT2, "y"]],
+        )
+    ]
+    # If neither point is not NaN
+    all_bad_mask = np.logical_and(pt1_nans, pt2_nans)
+    centroid[all_bad_mask, :] = np.nan
+
+    centroid_dict = {}
+    centroid_dict["position_x"] = centroid[0]
+    centroid_dict["position_y"] = centroid[1]
+    return centroid_dict
+
+
+def one_pt_centroid(pos_df, **params):
+    idx = pd.IndexSlice
+    PT1 = params.pop("point1", None)
+    centroid = pos_df.loc[:, idx[PT1, ("x", "y")]].to_numpy()
+    centroid_dict = {}
+    centroid_dict["position_x"] = centroid[:, 0]
+    centroid_dict["position_y"] = centroid[:, 1]
+    return centroid_dict
 
 
 _key_to_func_dict = {
-    'four_led_centroid': four_led_centroid,
-    'two_pt_centroid': two_pt_centroid,
-    'one_pt_centroid': one_pt_centroid,
+    "four_led_centroid": four_led_centroid,
+    "two_pt_centroid": two_pt_centroid,
+    "one_pt_centroid": one_pt_centroid,
 }
